c12510c2f0408f1a264a958b0cebfb6600e86881
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-10 15:14:43 -0700

    Reduce the dataset:

--- loop1.cc
+++ loop1.cc
@@ -1,25 +1,46 @@
+// #include <iostream>
+
+// int64_t calc_sum(int64_t *arr, int64_t len) {
+//     int64_t sum = 0;
+//     for (int64_t i = 0; i < len; i++) {
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+
+// int main() {
+//     int64_t len = 1e8;
+//     int64_t *arr = new int64_t[len];
+//     for (int64_t i = 0; i < len; i++) {
+//         arr[i] = i;
+//     }
+
+//     for (int64_t i = 0; i < 4*1e2; i++) {
+//         std::cout << i << ": ";
+//         std::cout << calc_sum(arr, len) << std::endl;
+//     }
+
+//     delete[] arr;
+// }
+
 #include <iostream>
 
-int64_t calc_sum(int64_t *arr, int64_t len) {
-    int64_t sum = 0;
-    for (int64_t i = 0; i < len; i++) {
-        sum += arr[i];
-    }
-    return sum;
-}
-
 int main() {
-    int64_t len = 1e8;
-    int64_t *arr = new int64_t[len];
-    for (int64_t i = 0; i < len; i++) {
-        arr[i] = i;
+    // Create two arrays of integers
+    int array1[1000000];
+    int array2[1000000];
+
+    // Initialize the arrays
+    for (int i = 0; i < 1000000; i++) {
+        array1[i] = i;
+        array2[i] = i * 2;
     }
 
-    for (int64_t i = 0; i < 4*1e2; i++) {
-        std::cout << i << ": ";
-        std::cout << calc_sum(arr, len) << std::endl;
+    // Access the elements of the arrays in a loop
+    for (int i = 0; i < 1000000; i++) {
+        int value1 = array1[i];
+        int value2 = array2[i];
     }
 
-    delete[] arr;
+    return 0;
 }
-
