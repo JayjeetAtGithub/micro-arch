37916db66a2b2d8de5e9c0dc215e9fe58b745ce9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-10 14:15:59 -0700

    Fixes

--- loop1.cc
+++ loop1.cc
@@ -2,14 +2,14 @@
 
 int main() {
     int *arr = new int[10];
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 1000000; i++) {
         arr[i] = i;
     }
 
     // read every element in the array
-    for (int i = 0; i < 10; i++) {
-        int v = arr[i] + 1;
-        std::cout << v << std::endl;
-    }
+    // for (int i = 0; i < 10; i++) {
+    //     int v = arr[i] + 1;
+    //     std::cout << v << std::endl;
+    // }
 }
 
