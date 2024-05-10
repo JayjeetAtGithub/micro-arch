89488537dc5ba10db93327851958b08b25ddda00
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-10 14:30:32 -0700

    Free up the memory

--- loop1.cc
+++ loop1.cc
@@ -1,17 +1,17 @@
 #include <iostream>
 
 int main() {
-    int64_t len = 1e10;
+    int64_t len = 0.75*1e10;
     int64_t *arr = new int64_t[len];
     for (int64_t i = 0; i < len; i++) {
         arr[i] = i;
     }
 
-    // read every element in the array
-    // for (int i = 0; i < 10; i++) {
-    //     int v = arr[i] + 1;
-    //     std::cout << v << std::endl;
-    // }
+    read every element in the array
+    for (int i = 0; i < 10; i++) {
+        int v = arr[i] + 1;
+        std::cout << v << std::endl;
+    }
 
     delete[] arr;
 }
