15b9518bb3e0fa0433484b8db2e5cebb7a57ca41
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-10 14:55:06 -0700

    Reduce the dataset:

--- loop1.cc
+++ loop1.cc
@@ -9,13 +9,13 @@ int64_t calc_sum(int64_t *arr, int64_t len) {
 }
 
 int main() {
-    int64_t len = 1e9;
+    int64_t len = 1e7;
     int64_t *arr = new int64_t[len];
     for (int64_t i = 0; i < len; i++) {
         arr[i] = i;
     }
 
-    for (int64_t i = 0; i < 1e3; i++) {
+    for (int64_t i = 0; i < 1e2; i++) {
         std::cout << i << ": ";
         std::cout << calc_sum(arr, len) << std::endl;
     }
